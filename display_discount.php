9cb6b35309707b7e911601e9a0bac6d132b4dab1
sp2254 <sp2254@njit.edu>
2017-02-12 16:15:55 -0500

    updated filter_input function

--- display_discount.php
+++ display_discount.php
@@ -7,6 +7,12 @@
   $list_price = $_POST['list_price'];
   $discount_percent = $_POST['discount_percent'];
 
+
+  $product_description = filter_input(INPUT_POST, 'product_description');
+  $list_price = filter_input(INPUT_POST, 'list_price');
+  $discount_percent = filter_input(INPUT_POST, 'discount_percent');
+
+
 ?>
 <!DOCTYPE html>
 <html>
