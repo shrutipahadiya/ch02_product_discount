7b199e2eeb2b417ea1fbf8ca8612a888daf07083
sp2254 <sp2254@njit.edu>
2017-02-12 16:13:33 -0500

    updated echo statements

--- display_discount.php
+++ display_discount.php
@@ -18,11 +18,11 @@
  <main>
  <h1>This page is under construction</h1>
  <label>Product Description:</label>
- <span><?php echo '$product_desc'; ?></span><br>
+ <span><?php echo $product_desc; ?></span><br>
  <label>List Price:</label>
- <span><?php echo '$List_price'; ?></span><br>
+ <span><?php echo $list_price; ?></span><br>
  <label>Standard Discount:</label>
- <span><?php echo '$discount_percent'; ?></span><br>
+ <span><?php echo $discount_percent; ?></span><br>
  <label>Discount Amount:</label>
  <span><?php echo ''; ?></span><br>
  <label>Discount Price:</label>
