05142773eca27e67f0505d53854bd6186753607b
sp2254 <sp2254@njit.edu>
2017-02-12 15:19:43 -0500

    code for formatting currency format

--- display_discount.php
+++ display_discount.php
@@ -19,7 +19,7 @@
        $list_price_f = "$".number_format($list_price, 2);
        $discount_percent_f = $discount_percent."%";
        $discount_f = "$".number_format($discount, 2);
-       $discount_price_f = "$".number_format($discount_price, 2);*/
+       $discount_price_f = "$".number_format($discount_price, 2);
   ?>
 <!DOCTYPE html>
 <html>
