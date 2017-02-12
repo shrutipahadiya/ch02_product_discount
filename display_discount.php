adc80dd1542c81bc737025d1d4f3a57a0f051b6b
sp2254 <sp2254@njit.edu>
2017-02-12 18:04:16 -0500

    updated list value field

--- display_discount.php
+++ display_discount.php
@@ -1,35 +1,23 @@
 
 <?php
 
-
-  /* Code to get request data from html form
- $product_desc = $_POST['product_description'];
- $list_price = $_POST['list_price'];
- $discount_percent = $_POST['discount_percent'];*/
-
-  //code to use filter_input function
+ //code to use filter_input function
   $product_desc = filter_input(INPUT_POST, 'product_description');
-  $list_price_new = filter_input(INPUT_POST, 'list_price');
+  $list_price_value = filter_input(INPUT_POST, 'list_price');
   $discount_percent = filter_input(INPUT_POST, 'discount_percent');
 
 
  //calculate the discount amount and discount price
-   $discount_amount = $list_price_new * $discount_percent * .01;
-   $discount_price = $list_price_new - $discount_percent;
+   $discount_amount = $list_price_value * $discount_percent * .01;
+   $discount_price = $list_price_value - $discount_percent;
 
 
 
  //Code for currency format
-    $list_price_f = "$".number_format($list_price_new, 2);
+    $list_price_f = "$".number_format($list_price_value, 2);
     $discount_percent_f = $discount_percent."%";
     $discount_amount_f = "$".number_format($discount_amount, 2);
     $discount_price_f = "$".number_format($discount_price, 2);
-
-
-  /* code for htmlspecialchars function
-   $product_description_new = htmlspecialchars($product_desc);
-   $list_price_new = htmlspecialchars($list_price_f);
-   $discount_percent_new = htmlspecialchars($discount_percent_f);*/
 
 ?>
 <!DOCTYPE html>
@@ -44,7 +32,7 @@
  <label>Product Description:</label>
  <span><?php echo htmlspecialchars($product_desc);?></span><br>
  <label>List Price:</label> 
- <span><?php echo htmlspecialchars($list_price_f);?></span><br>
+ <span><?php echo $list_price_value;?></span><br>
  <label>Standard Discount:</label> 
  <span><?php echo htmlspecialchars($discount_percent_f);?></span><br>
  <label>Discount Amount:</label> 
