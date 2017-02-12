7703d4e84864683b2ec6f24d81d65653c350f5be
sp2254 <sp2254@njit.edu>
2017-02-12 17:20:13 -0500

    updated with htmlspecialchar function

--- display_discount.php
+++ display_discount.php
@@ -3,29 +3,33 @@
 
 
   // Code to get request data from html form
-  $product_desc = $_POST['product_description'];
-  $list_price = $_POST['list_price'];
-  $discount_percent = $_POST['discount_percent'];
+// $product_desc = $_POST['product_description'];
+// $list_price = $_POST['list_price'];
+// $discount_percent = $_POST['discount_percent'];
 
   //code to use filter_input function
-  $product_description = filter_input(INPUT_POST, 'product_description');
-  $list_price = filter_input(INPUT_POST, 'list_price');
+  $product_desc = filter_input(INPUT_POST, 'product_description');
+  $list_price_new = filter_input(INPUT_POST, 'list_price');
   $discount_percent = filter_input(INPUT_POST, 'discount_percent');
 
 
  // calculate the discount amount and discount price
-   $discount_amount = $list_price * $discount_percent * .01;
-   $discount_price = $list_price - $discount;
+   $discount_amount = $list_price_new * $discount_percent * .01;
+   $discount_price = $list_price_new - $discount_percent;
 
 
 
  // Code for currency format
-    $list_price = "$".number_format($list_price, 2);
-    $discount_percent = $discount_percent."%";
-    $discount_amount = "$".number_format($discount_amount, 2);
-    $discount_price = "$".number_format($discount_price, 2);
+    $list_price_f = "$".number_format($list_price_new, 2);
+    $discount_percent_f = $discount_percent."%";
+    $discount_amount_f = "$".number_format($discount_amount, 2);
+    $discount_price_f = "$".number_format($discount_price, 2);
 
 
+  // code for htmlspecialchars function
+//   $product_description_new = htmlspecialchars($product_desc);
+  // $list_price_new = htmlspecialchars($list_price_f);
+   //$discount_percent_new = htmlspecialchars($discount_percent_f);
 
 
 ?>
@@ -39,16 +43,17 @@
  <main>
  <h1>This page is under construction</h1>
  <label>Product Description:</label>
- <span><?php echo htmlspecialchars($product_desc); ?></span><br>
- <label>List Price:</label>
- <span><?php echo htmlspecialchars($list_price); ?></span><br>
- <label>Standard Discount:</label>
- <span><?php echo htmlspecialchars($discount_percent); ?></span><br>
- <label>Discount Amount:</label>
- <span><?php echo $discount_amount; ?></span><br>
- <label>Discount Price:</label>
-  <span><?php echo $discount_price; ?></span><br>
+ <span><?php echo htmlspecialchars($product_desc);?></span><br>
+ <label>List Price:</label> 
+ <span><?php echo htmlspecialchars($list_price_f); ?></span><br>
+ <label>Standard Discount:</label> 
+ <span><?php echo htmlspecialchars($discount_percent_f);?></span><br>
+ <label>Discount Amount:</label> 
+ <span><?php echo $discount_amount_f; ?></span><br>
+ <label>Discount Price:</label> 
+ <span><?php echo $discount_price_f; ?></span><br>
  </main>
 </body>
+
 
 </html>
